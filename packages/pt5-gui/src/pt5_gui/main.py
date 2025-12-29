21ec75d0eff19f249735b322c7a932b436d6a96b
Dan Homola <dan.homola@hotmail.cz>
2025-12-29 15:58:01 +0100

    refactor: better conversion
    Convert the file on open, this allows us to draw it as well.

--- packages/pt5-gui/src/pt5_gui/main.py
+++ packages/pt5-gui/src/pt5_gui/main.py
@@ -1,3 +1,4 @@
+import math
 import tkinter.filedialog
 from pathlib import Path
 from tkinter import ttk
@@ -13,32 +14,33 @@ class App:
 
         self.source_filename = tkinter.StringVar()
         self.target_filename = tkinter.StringVar()
+        self.parsed: NcpFile | None = None
 
         frm = ttk.Frame(master, padding=10, width=800, height=600)
         frm.grid()
 
         ttk.Label(frm, text="Source file").grid(column=0, row=0)
-        ttk.Label(frm, textvariable=self.source_filename).grid(column=1, row=0)
+        ttk.Label(frm, textvariable=self.source_filename).grid(column=1, row=0, columnspan=3)
 
         ttk.Label(frm, text="Target file").grid(column=0, row=1)
-        ttk.Label(frm, textvariable=self.target_filename).grid(column=1, row=1)
+        ttk.Label(frm, textvariable=self.target_filename).grid(column=1, row=1, columnspan=3)
 
-        ttk.Button(frm, text="Select source file", command=self.open_file).grid(column=0, row=2)
+        ttk.Button(frm, text="Select source file", command=self.open_ncp_file).grid(column=0, row=2)
         ttk.Button(frm, text="Convert", command=self.convert).grid(column=1, row=2)
-        ttk.Button(frm, text="Quit", command=master.destroy).grid(column=2, row=2)
+        ttk.Button(frm, text="Quit", command=master.destroy).grid(column=3, row=2)
 
-    def open_file(self) -> None:
+    def open_ncp_file(self) -> None:
         source_filename = tkinter.filedialog.askopenfilename(filetypes=[("NCP files", "*.ncp")])
         target_filename = str(Path(source_filename).with_suffix(".pt5"))
         self.source_filename.set(source_filename)
         self.target_filename.set(target_filename)
+        with open(self.source_filename.get()) as src:
+            self.parsed = NcpFile.parse(src)
 
     def convert(self) -> None:
-        with open(self.source_filename.get()) as src:
-            parsed = NcpFile.parse(src.readlines())
-            converted = ncp_to_pt5(parsed)
-            with open(self.target_filename.get(), "w") as target:
-                target.writelines(converted.serialize())
+        converted = ncp_to_pt5(self.parsed)
+        with open(self.target_filename.get(), "w") as target:
+            target.writelines(converted.serialize())
 
 
 root = tkinter.Tk()
